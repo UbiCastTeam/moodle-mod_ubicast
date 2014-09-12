d58074007e09fee8df0d79314d0e5db1ffb3f5f8
Stéphane Diemer <stephane.diemer@gmail.com>
2014-09-12 18:47:21 +0200

    Fixed some minor bugs in views.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $course = $DB->get_record('course', array('id'=>$id), '*', MUST_EXIST);
 require_course_login($course, true);
 $PAGE->set_pagelayout('incourse');
 
-add_to_log($course->id, 'easycastms', 'view all', "index.php?id=$course->id", '');
+add_to_log($course->id, 'easycastms', 'view all', 'index.php?id=$course->id', '');
 
 $strmedias       = get_string('medias', 'easycastms');
 $strsectionname  = get_string('sectionname', 'format_'.$course->format);
@@ -31,7 +31,7 @@ $PAGE->set_heading($course->fullname);
 $PAGE->navbar->add($strmedias);
 echo $OUTPUT->header();
 
-$easycastms_medias = get_all_instances_in_course('easycastms', $course)
+$easycastms_medias = get_all_instances_in_course('easycastms', $course);
 if (!$easycastms_medias) {
     notice(get_string('thereareno', 'moodle', $strmedias), "$CFG->wwwroot/course/view.php?id=$course->id");
     exit;
@@ -69,22 +69,22 @@ foreach ($easycastms_medias as $easycastms_media) {
             $currentsection = $easycastms_media->section;
         }
     } else {
-        $printsection = '<span class="smallinfo">'.userdate($easycastms_media->timemodified)."</span>";
+        $printsection = '<span class="smallinfo">'.userdate($easycastms_media->timemodified).'</span>';
     }
 
     $extra = empty($cm->extra) ? '' : $cm->extra;
     $media_type = 'video';
-    if ($easycastms_media[0] == 'l') {
-        $media_type = 'live'
-    } else if ($easycastms_media[0] == 'p') {
-        $media_type = 'photos'
+    if ($easycastms_media->mediaid[0] == 'l') {
+        $media_type = 'live';
+    } else if ($easycastms_media->mediaid[0] == 'p') {
+        $media_type = 'photos';
     }
     $icon = '<img src="'.$OUTPUT->pix_url($media_type).'" class="activityicon" alt="'.$media_type.'" /> ';
 
     $class = $easycastms_media->visible ? '' : 'class="dimmed"'; // hidden modules are dimmed
     $table->data[] = array(
         $printsection,
-        "<a $class $extra href=\"view.php?id=$cm->id\">".$icon.format_string($easycastms_media->name)."</a>",
+        '<a $class $extra href="view.php?id=$cm->id">'.$icon.format_string($easycastms_media->name).'</a>',
         format_module_intro('easycastms', $easycastms_media, $cm->id));
 }
 
