e428e847208928d2a90eb342eacca79098ebfb24
Stéphane Diemer <stephane.diemer@ubicast.eu>
2015-03-17 10:09:22 +0100

    Replaced module by plugin in version file.

--- version.php
+++ version.php
@@ -10,9 +10,9 @@
 
 if (!defined('MOODLE_INTERNAL')) { die('Direct access to this script is forbidden.'); }
 
-$module->version   = 2015031700;                 // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2014051200; // 2.7.0        // Requires this Moodle version
-$module->component = 'mod_easycastms';           // Full name of the plugin (used for diagnostics)
-$module->cron      = 0;
-$module->release   = '1.18 (Build: 2015031700)'; // Human-readable version name
-$module->maturity  = MATURITY_STABLE;            // Maturity of module
+$plugin->version   = 2015031700;                 // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014051200; // 2.7.0        // Requires this Moodle version
+$plugin->component = 'mod_easycastms';           // Full name of the plugin (used for diagnostics)
+$plugin->release   = '1.18 (Build: 2015031700)'; // Human-readable version name
+$plugin->maturity  = MATURITY_STABLE;            // Maturity of module
+$plugin->cron      = 0;
