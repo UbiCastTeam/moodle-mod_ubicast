a34a6648af43a6fd9aeb2f5764cc72c17a34455e
achauviere24 <a.chauviere@ingenium-elearning.com>
2022-02-03 09:59:35 +0100

    fix codechecker

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -1,29 +1,56 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Mobile output class for ubicast
+ *
+ * @package    mod_ubicast
+ */
 namespace mod_ubicast\output;
-
-defined('MOODLE_INTERNAL') || die();
 
 use context_module;
 
+/**
+ * Mobile output class for bigbluebuttonbn
+ *
+ * @package    mod_ubicast
+ */
 class mobile {
-
+    /**
+     * Returns the ubicast course view for the mobile app.
+     *
+     * @param mixed $args
+     * @throws \moodle_exception
+     * @return array HTML, javascript and other data.
+     */
     public static function mobile_course_view($args) {
-        global $DB,$OUTPUT;
+        global $DB, $OUTPUT;
 
         $args = (object) $args;
         $cm = get_coursemodule_from_id('ubicast', $args->cmid);
 
         if (!$ubicastresource = $DB->get_record('ubicast', array('id' => $cm->instance))) {
-            print_error('course module is incorrect');
+            throw new moodle_exception('course module is incorrect');
         }
 
         $config = get_config('ubicast');
         $key = $config->ubicast_ltikey;
         $secret = $config->ubicast_ltisecret;
-
         $iframeurl = $config->ubicast_url.'/permalink/'.$ubicastresource->mediaid.'/iframe/?login=no';
-        $data=['iframeurl' => $iframeurl];
+        $data = ['iframeurl' => $iframeurl];
         return [
             'templates' => [
                 [
--- db/mobile.php
+++ db/mobile.php
@@ -1,21 +1,43 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Mobile app definition for ubicast
+ *
+ * @package   mod_ubicast
+ */
+
+defined('MOODLE_INTERNAL') || die;
 
 $addons = array(
     "mod_ubicast" => array(
-        "handlers" => array( // Different places where the add-on will display content.
-            'coursechoicegroup' => array( // Handler unique name (can be anything)
+        "handlers" => array(
+            'coursechoicegroup' => array(
                 'displaydata' => array(
                     'title' => 'pluginname',
                     'icon' => $CFG->wwwroot . '/mod/ubicast/pix/icon.svg',
                     'class' => '',
                 ),
-                'delegate' => 'CoreCourseModuleDelegate', // Delegate (where to display the link to the add-on)
-                'method' => 'mobile_course_view', // Main function in \mod_choicegroup\output\mobile
+                'delegate' => 'CoreCourseModuleDelegate',
+                'method' => 'mobile_course_view',
                 'styles' => array(
                     'url' => $CFG->wwwroot . '/mod/ubicast/css/style_app.css',
                     'version' => '0.4',
                 ),
-                'displayrefresh' => true, // Hide default refresh button, a custom one will be used.
+                'displayrefresh' => true,
             )
         ),
         'lang' => array(
